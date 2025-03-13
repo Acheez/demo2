cd2561ff6b241e5781ec7b104e03e2863767ef1b
Archiecon <archili.robakidze@alcon.com>
2025-03-13 20:57:26 +0400

    add: class for the pdf parser

---
+++ microservice_pdf/pdf_parser.py
@@ -0,0 +1,9 @@
+import io
+import PyPDF2
+
+class PdfParser:
+    """Handles PDF file reading and text extraction using PyPDF2."""
+
+    @staticmethod
+    def parse(pdf_bytes: bytes) -> str:
+        pass
